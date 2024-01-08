564e1f5c1633aac73c256a480ae560d9ff2c18bd
Liam <liamsuu14974@gmail.com>
2024-01-08 22:03:32 +0000

    Add numbered calculator buttons

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // object just holds all the functionality.
+// might need to change so that it takes an array of the previously typed stuff and only minus
+// the number that follows with event listener.
 const calcFunction = {
   add: (...params) => {
     let sum = 0;
@@ -7,4 +9,16 @@ const calcFunction = {
     });
     return sum;
   },
+
+  createButtons: () => {
+    const container = document.querySelector(".main-container");
+    for (let x = 0; x !== 10; x++) {
+      const buttons = document.createElement("button");
+      buttons.className = "buttons";
+      buttons.textContent = `${x}`;
+      container.appendChild(buttons);
+    }
+  },
 };
+
+calcFunction.createButtons();
