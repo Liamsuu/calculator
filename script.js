5ceaa95e61666df70bb46ddf562f4818de42f939
Liam <liamsuu14974@gmail.com>
2024-01-08 21:45:01 +0000

    Create container, add heading and some basic style

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+// object just holds all the functionality.
+const calcFunction = {
+  add: (...params) => {
+    let sum = 0;
+    params.forEach((number) => {
+      sum += number;
+    });
+    return sum;
+  },
+};
