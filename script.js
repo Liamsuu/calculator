44d7779110ad554e7ccb8ded8b90eb01a3515fdf
Liam <liamsuu14974@gmail.com>
2024-01-09 16:49:46 +0000

    Add display text to update functionality, clear and result buttons
    The text updates as buttons are clicked by grabbing button text
    content. This will later be turned from string into an equation.
    Both equals and clear buttons are added, functionality to be added
    later.

--- script.js
+++ script.js
@@ -45,6 +45,10 @@ const calcFunction = {
     });
     return sum;
   },
+
+  /* perhaps for this createButtons function I should make it like a grid with 3 buttons 
+  on each div row like in the etch a sketch program, look at the program on how to do tomorrow */
+
   createButtons: () => {
     const container = document.querySelector("#buttons-container");
     for (let x = 0; x !== 10; x++) {
@@ -72,6 +76,21 @@ const calcFunction = {
         return "ERROR";
     }
   },
+
+  // create calculate function that simply grabs display text and turns it into numbers to calculate
+  // then replace text content with that.
+};
+
+const otherFunctionality = {
+  // update so operators cannot be appended one after another such as "+-" only allowing a num after
+  displayTextUpdater: (nodelist) => {
+    nodelist.forEach((button) => {
+      button.addEventListener("click", () => {
+        const buttonNumber = button.textContent;
+        display.textContent += buttonNumber;
+      });
+    });
+  },
 };
 
 // example testing below
@@ -80,5 +99,11 @@ let secondNum = 4;
 let operator = "-";
 
 console.log(calcFunction.operate(operator, firstNum, secondNum));
-
 calcFunction.createButtons();
+const buttons = document.querySelectorAll(".buttons");
+const outerButtons = document.querySelectorAll(".outer-buttons");
+const display = document.querySelector("#display");
+let displayText;
+
+otherFunctionality.displayTextUpdater(buttons);
+otherFunctionality.displayTextUpdater(outerButtons);
